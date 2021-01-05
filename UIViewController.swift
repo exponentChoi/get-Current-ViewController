9b6951789161aa1b00b62378cefb0edc92414be3
exponentChoi <54607538+exponentChoi@users.noreply.github.com>
2021-01-05 17:57:12 +0900

    Create UIViewController.swift

---
+++ UIViewController.swift
@@ -0,0 +1,25 @@
+/  UIViewController.swift
+//  UIViewController
+//
+//  Created by 최지수 on 2021/01/05.
+//
+
+import UIKit
+
+extension UIViewController {
+    /// get the current displaying UIViewController
+    static func getCurrentViewController(_ vc: UIViewController) -> UIViewController? {
+        if let pvc = vc.presentedViewController {
+            return getCurrentViewController(pvc)
+        } else if let svc = vc as? UISplitViewController, svc.viewControllers.count > 0 {
+            return getCurrentViewController(svc.viewControllers.last ?? vc)
+        } else if let nvc = vc as? UINavigationController, nvc.viewControllers.count > 0 {
+            return getCurrentViewController(nvc.topViewController ?? vc)
+        } else if let tbc = vc as? UITabBarController {
+            if let svc = tbc.selectedViewController {
+                return getCurrentViewController(svc)
+            }
+        }
+        return vc
+    }
+}
